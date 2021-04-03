62fd20121338090dbccc0c0715d257ab781281f5
Ivanncho <jovanovski.ivancho@yahoo.com>
2021-04-03 16:15:10 +0200

    ...

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -31,6 +31,16 @@ public class CustomerController {
 		return "list-customers";
 	}
 	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		//create model attribute to bind the data 
+		Customer customer = new Customer();
+		model.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
+	
 	
 	
 }
