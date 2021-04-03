151f37b7ad3c3161b0b14610c6d25f307fef2c93
Ivanncho <jovanovski.ivancho@yahoo.com>
2021-04-03 15:48:28 +0200

    Adding service layer

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,28 +5,29 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	//need to inject DAO in this controller
+	//need to inject customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
-		//get customers from the DAO
-		List<Customer> customer = customerDAO.getCustomers();
+		//get customers from the service
+		List<Customer> customer = customerService.getCustomers();
+		
 		//add the customers to the model
-		model.addAttribute("customers", customer);
-		
-		
+		model.addAttribute("customers", customer);		
 		return "list-customers";
 	}
 	
